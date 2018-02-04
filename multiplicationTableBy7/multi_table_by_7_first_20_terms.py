3d3f45a52a316fa37533b819ab9255c9d4700e5b
cyaoyapi <cyaoyapi@gmail.com>
2018-02-04 12:25:59 +0000

    Multiplication table of 7. The first 20 terms.

---
+++ multiplicationTableBy7/multi_table_by_7_first_20_terms.py
@@ -0,0 +1,8 @@
+# This program display the firt 20 terms of the multiplication of 7
+
+print("Multiplication table of 7 === First 20 terms")
+print("================================================")
+term = 1
+while term < 21:
+	print(" 7 *", term,"=",7*term)
+	term += 1
