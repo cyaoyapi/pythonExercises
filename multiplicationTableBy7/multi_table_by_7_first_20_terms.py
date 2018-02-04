423be9191cf05f51925e2f12b1f019a92758a34f
cyaoyapi <cyaoyapi@gmail.com>
2018-02-04 14:44:22 +0000

    Add (*) when the term is multiple of 3.

--- multiplicationTableBy7/multi_table_by_7_first_20_terms.py
+++ multiplicationTableBy7/multi_table_by_7_first_20_terms.py
@@ -1,8 +1,13 @@
-# This program display the firt 20 terms of the multiplication of 7
+# This program display the firt 20 terms of the multiplication of 7. Add (*) when the term is a mulptuple of 3.
 
 print("Multiplication table of 7 === First 20 terms")
 print("================================================")
 term = 1
 while term < 21:
-	print(" 7 *", term,"=",7*term)
+	result = 7 * term
+	print(result, end="")
+	if result % 3 == 0:
+		print("(*)", end="")
+	print(end=" ")
 	term += 1
+print("")
