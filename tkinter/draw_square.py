1a39f563dda643ce0e26005b654f3b1ced606637
cyaoyapi <cyaoyapi@gmail.com>
2018-02-04 23:32:40 +0000

    Comments about using signal.pause()

--- tkinter/draw_square.py
+++ tkinter/draw_square.py
@@ -1,6 +1,6 @@
 # This program draw a square of certain side according the entered value by the user
 
-import signal
+#import signal
 from turtle import *  
 
 side = int(input("Enter the side of the square :\n"))
@@ -12,6 +12,7 @@ while cpt <= 3:
 	left(90)
 	forward(side)
 	cpt += 1
+# We can put the program on pause using signal.pause()
 stop = input("Type ENTER to stop the program :\n")
 exit()
 
