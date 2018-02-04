114e78ec910fc02ebd5aab090837e3972b0d5d54
cyaoyapi <cyaoyapi@gmail.com>
2018-02-04 13:25:44 +0000

    Volume of rectangular parallelepiped.

---
+++ rectangularParallelepipedVolume/volume_of_parallelipiped.py
@@ -0,0 +1,8 @@
+# This program calculates the volume of a rectangular parallelepiped
+# Volume = Length * width * height
+
+length = int(input("Enter the length of the parallelepiped\n"))
+width = int(input("Enter the width of the parallelepiped\n"))
+height = int(input("Enter the height of the parallelepiped\n"))
+volume = length * width * height
+print("The volume of the parallelepiped is {}".format(volume))
