6405633289e6a132d3822c7482fcdaaff7bd2d7e
cyaoyapi <cyaoyapi@gmail.com>
2018-02-05 13:23:35 +0000

    refactoring of the code

--- tkinter/draw_square.py
+++ tkinter/draw_square.py
@@ -6,11 +6,10 @@ from turtle import *
 side = int(input("Enter the side of the square :\n"))
 color('blue')
 width(5)
-forward(side)
 cpt = 1
-while cpt <= 3:
-	left(90)
+while cpt <= 4:	
 	forward(side)
+	left(90)
 	cpt += 1
 # We can put the program on pause using signal.pause()
 stop = input("Type ENTER to stop the program :\n")
