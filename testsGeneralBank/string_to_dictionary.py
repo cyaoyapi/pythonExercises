612667f68e8602665adea06cf83a202bd424e827
cyaoyapi <cyaoyapi@gmail.com>
2018-02-07 16:24:30 +0000

    Adding comments about using json.loads instead of ast.literal_eval

--- testsGeneralBank/string_to_dictionary.py
+++ testsGeneralBank/string_to_dictionary.py
@@ -1,13 +1,16 @@
-import ast
+import ast # Or import json
 
 def string_to_dictionary(param_string):
 
 	""" Change a string formated like a dictionnary to a real dictionary """
 
-	return ast.literal_eval(param_string)
+	return ast.literal_eval(param_string) # return json.loads(param_string)
+
 
 # The program
 
-family_dict = "{'Father':'Cyrille','Mother':'Agnès','Daughter':'Dabira'}"
+    # If we use json, the delimiter must be simple quotes ''
+family_dict = "{'Father':'Cyrille','Mother':'Agnès','Daughter':'Dabira'}" 
+
 
 print(string_to_dictionary(family_dict))
